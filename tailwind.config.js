affc32ddd779bd5798a5de530a37d8ee6985db9a
Grini Andrei <andrei.grini@carasent.com>
2024-10-14 16:32:43 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const Path = require('path')
+const pwd = process.env.PWD
+
+// We can add current project paths here
+const projectPaths = [
+    Path.join(pwd, '../capstone/templates/**/*.html'),
+    // add js file paths if you need
+]
+
+const contentPaths = [...projectPaths]
+console.log(`tailwindcss will scan ${contentPaths}`)
+
+module.exports = {
+    content: contentPaths,
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
